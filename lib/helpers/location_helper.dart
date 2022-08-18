151c5de4e2220aca69cb9064a2bb1bd01aea4ef5
bantawa04 <rahunn3@gmail.com>
2022-08-18 13:20:27 +0545

    13.17 Add google geolocation api

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,8 +1,20 @@
-const GOOGLE_API_KEY="AIzaSyB2R3TGlGaETV_0TnQ-oask0VlMP-oQq8U";
-const SECRET_KEY = "Cmsvx7H8ShMDEP_b41q0IL8ybp4=";
-class LocationHelper {
+import 'dart:convert';
+import "package:http/http.dart" as http;
 
-  static String generateLocationPreviewImage({required double latitude, required double longitude}){
-     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+const GOOGLE_API_KEY = "AIzaSyB2R3TGlGaETV_0TnQ-oask0VlMP-oQq8U";
+const SECRET_KEY = "Cmsvx7H8ShMDEP_b41q0IL8ybp4=";
+
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {required double latitude, required double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    var url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
+
+    final response = await http.get(Uri.parse(url));
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
